9a95c3e78aa9cec047e9a455b86c21d9898b358c
Harsh <h77776667777@gmail.com>
2025-08-26 16:52:54 +0530

    Update Complaint.js

--- models/Complaint.js
+++ models/Complaint.js
@@ -1,18 +1,16 @@
 import mongoose from "mongoose";
 
-const PhotoSchema = new mongoose.Schema({
-  filename: String,
-  originalname: String,
-  path: String,
-  mimetype: String,
-  size: Number
-}, { _id: false });
-
-const ComplaintSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  age: { type: Number, required: true },
-  problem: { type: String, required: true },
-  photo: { type: PhotoSchema, default: null }
-}, { timestamps: true });
+const ComplaintSchema = new mongoose.Schema(
+  {
+    name: String,
+    age: Number,
+    problem: String,
+    photo: {
+      data: Buffer,       // yaha actual binary image data store hoga
+      contentType: String // yaha file ka mimetype (image/png etc.)
+    },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Complaint", ComplaintSchema);
